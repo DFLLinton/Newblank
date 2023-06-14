779ee4e0abf28baefa3e679ba02b8022dae0d2b5
DFLLinton <dominiclinton@gmail.com>
2023-06-14 21:40:52 +0100

    Simplify function structure

--- myapp/src/ImageColumnLeft.js
+++ myapp/src/ImageColumnLeft.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 
 const ImageColumnLeft = (props) => {
-  const {image1Src} = props;
+  const {imageSrc} = props;
+console.log(imageSrc);
 
   return (
     <div className="associatedImage" id="image1">
-      <img src={image1Src} className="sampleImage" id="image1file" />
+            <img src={require('.//images/favanswer.jpg')} className="sampleImage" id="image1file" />
+      <img src={require('.//images/vinos.jpg')} className="sampleImage" id="image1file" />
       <div className="sampleText" id="leftText">
       </div>
     </div>
--- myapp/src/Project.js
+++ myapp/src/Project.js
@@ -4,32 +4,29 @@ import ImageColumnLeft from './ImageColumnLeft.js';
 import ImageColumnRight from './ImageColumnRight.js';
 
 export default function Project() {
-  const [image1Visible, setImage1Visible] = useState(false);
-  const [image1Src, setImage1Src] = useState('myapp/public/images/favanswer.jpg');
-  const [image2Visible, setImage2Visible] = useState(false);
-  const [image2Src, setImage2Src] = useState('');
+  const [imageSrc, setImageSrc] = useState('.//images/vinos.jpg');
   const [leftTextContent, setLeftTextContent] = useState('');
   const [rightTextContent, setRightTextContent] = useState('');
 
   const Paragraphs = {
-    1: ' - Pair programming in a team of seven <br> developers deploying a new mobile <br> application.<br> - Integrating and designing twelve front-end <br> React components and APIs via Github,<br>prioritising principles of object oriented<br>programming.',
+    1: ' - Pair programming  in a team of seven <br> developers deploying a new mobile <br> application.<br> - Integrating and designing twelve front-end <br> React components and APIs via Github,<br>prioritising principles of object oriented<br>programming.',
     2: ' - Managed web development for new Soho-based <br> Bar Kroketa.<br> - The process involved communicating in a<br>team of eight designers, entrepreneurs and<br>advertising executives to agree on a viable<br>digital presence.<br> - Anticipated server issues with web<br>deployment and suggested solutions allowing<br>us to launch a five page, dynamic website<br>within stringent deadlines.',
-    3: 'Creating a website using PHP, CSS, HTML, and JavaScript involves combining front-end and back-end technologies to build the structure, style, and interactivity. Deploying it on an FTP server requires configuring file transfers and ensuring proper server setup for PHP execution. Attention to file organization and testing is crucial for a successful online presence.',
+    3: 'Creating a website  using PHP, CSS, HTML, and JavaScript involves combining front-end and back-end technologies to build the structure, style, and interactivity. Deploying it on an FTP server requires configuring file transfers and ensuring proper server setup for PHP execution. Attention to file organization and testing is crucial for a successful online presence.',
     4: 'Created Machine Learning Algorithm',
     5: ' - Developed own hypotheses and programmed<br>new experimental technique using Bonvision<br>interface.<br> - My paradigm has been adopted by subsequent<br>researchers at UCL demonstrating its<br>technical aptitude and versatility.<br> - Presented progress and discussed findings<br>at weekly meetings with both senior<br>researchers and software developer, Goncalo<br>Lopes.'
   };
 
   function showPicture(x) {
     if (x === 'favourite') {
-      setImage1Src('../favanswer.jpg');
+      setImageSrc('favourite');
     } else if (x === 'kroketa') {
-      setImage2Src('../kroketa.jpg');
+      setImageSrc('kroketa');
     } else if (x === 'vinos') {
-      setImage2Src('../vinos.jpg');
+      setImageSrc('vinos');
     } else if (x === 'gender') {
-      setImage1Src('../sample.jpeg');
+      setImageSrc('gender');
     } else if (x === 'animacy') {
-      setImage1Src('../animacy.png');
+      setImageSrc('animacy');
     }
   }
 
@@ -37,8 +34,8 @@ export default function Project() {
 
   return (
     <div>
-    <ImageColumnLeft image1Src={image1Src} />
- <ImageColumnRight image2Src={image2Src}/>
+    <ImageColumnLeft image1Src={imageSrc} />
+ <ImageColumnRight image2Src={imageSrc}/>
     <div id="column">
       <div
         className="projectDiv"
